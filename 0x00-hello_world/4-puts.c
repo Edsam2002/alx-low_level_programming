f396107fbffde17507f46f62215a40c37bdf4ccb
Edsam2002 <edsamuel1st@gmail.com>
2022-04-08 09:04:44 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-main()
+/**
+ *  * main - The containing function of this programme
+ *   * Return: this gives 0 if sucessful
+ *    */
+int main(void)
 {
-		puts("Programming is like building a multilingual puzzle\n");
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
