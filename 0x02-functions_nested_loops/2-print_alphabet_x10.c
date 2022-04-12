dbdad35b19e1fd4a854b4ecbf7b11a7ffdf88198
Edsam2002 <edsamuel1st@gmail.com>
2022-04-12 08:19:39 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_alphabet - print alphabet lowercase
+ *
+ *Return: ALways 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int num_alphabet;
+	char alphabet;
+
+	for (num_alphabet = 0; num_alphabet < 10; num_alphabet++)
+	{
+	for (alphabet = 'a'; alphabet < 'z'; alphabet++)
+	{
+  	_putchar(alphabet);
+	}
+
+	_putchar('\n')
+	}
+}
+
+
+
