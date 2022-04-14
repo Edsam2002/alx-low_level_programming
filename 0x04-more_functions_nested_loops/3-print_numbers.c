f2cac8213b40fc714133974a908815d20bc8ef2b
Edsam2002 <edsamuel1st@gmail.com>
2022-04-14 08:32:37 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int s;
+	s = 0;
+	while (s < 10)
+	{
+	_putchar (s + '0');
+	s++;
+	}
+
+	_putchar ('\n')
+	
+}
+
+	
