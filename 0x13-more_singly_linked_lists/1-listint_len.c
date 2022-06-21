7ea1c5400c936c2ba514b025f48a7643c466c344
Edsam2002 <edsamuel1st@gmail.com>
2022-06-21 12:13:36 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include "lists.h"
+
 /**
  * count - count the number
  * pointer - points s to head pointer
  */
+
 size_t listint_len(const listint_t *h)
 {
-	size_t count;
+	size_t count = 0;
 	const listint_t *point = h;
 	
-	while (point -> != NULL)
+	while (point -> next != NULL)
 	{
 	point = point->next;		
 	count++;
 	}
-	return count;
+	return (count);
 }
