8223c6e1343d35847d49fb47a95b9d6798eeada8
Edsam2002 <edsamuel1st@gmail.com>
2022-06-28 05:50:07 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,26 +1,35 @@
 #include "main.h"
 
+/**
+ * create_file - creates a file
+ * @filename: param
+ * @text_content: param
+ * Return: int
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int o, wr, slen = 0;
-	char *buf;
+	int of, nletters, rwr;
 
-	if (filename == NULL)
-	return (-1);
+	if (!filename)
+		return (-1);
 
-	o = open(filename, 0_CREAT, 600)	
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-		len++;	
-	}
+	of = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	w = write(0, text_content, len);
+	if (of == -1)
+		return (-1);
 
-	if (0 == -1 || w == -1)
-	return (-1);
+	if (!text_content)
+		text_content = "";
 
-	close(0);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
-	return (-1);
+	rwr = write(of, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(of);
+
+	return (1);
 }
